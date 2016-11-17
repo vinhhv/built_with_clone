2f50ee975d98b1f5eb80cc245905739acb3b8cc0
Vinh Vu <kroomey@gmail.com>
2016-11-17 11:28:06 -0800

    Add Grape API configuration

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,9 @@ module BuiltWithClone
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Auto-load API and its subdirectories
+    config.paths.add 'app/api', glob: '**/*.rb'
+    config.autoload_paths += Dir["#{Rails.root}/app/api/*"]
   end
 end
