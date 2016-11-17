57381ce70dba77f7ad502e62e1803f2d1f2c5886
Vinh Vu <kroomey@gmail.com>
2016-11-17 12:51:35 -0800

    Add basic built with API

--- app/api/api/v1/built_with.rb
+++ app/api/api/v1/built_with.rb
@@ -1,3 +1,7 @@
+require 'json'
+require 'net/http'
+require 'uri'
+
 module API
   module V1
     class BuiltWith < Grape::API
@@ -9,7 +13,23 @@ module API
           requires :url, type: String, desc: 'URL'
         end
         get 'build_with' do
-          return 201
+          unless params[:url] =~ URI::regexp
+            return "Incorrect URL format"
+          end
+          uri = URI(params[:url])
+          resp = {}
+          resp['html'] = Net::HTTP.get(uri).force_encoding('ISO-8859-1').encode('UTF-8')
+          if Net::HTTP.get_response(uri.host, '/robots.txt').class == Net::HTTPOK
+            resp['has robot.txt'] = true
+          else
+            resp['has robot.txt'] = false
+          end
+          if resp['html'].include?('bootstrap.css')
+            resp['uses bootstrap'] = true
+          else
+            resp['uses bootstrap'] = false
+          end
+          return resp
         end
       end
 
